3827bfbba73905cff24ae2a144db249462491b41
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-12 12:45:22 +0530

    pages init

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Login from "./Login";
+import Register from "./Register";
+import About from "./About";
+import Cart from "./Cart";
+import Home from "./Home";
+import Orders from "./Orders";
+import Checkout from "./Checkout";
+import Products from "./Products";
+import Error from "./Error";
+
+export {
+    Login,
+    Register,
+    About,
+    Cart,
+    Home,
+    Orders,
+    Checkout,
+    Products,
+    Error
+}
