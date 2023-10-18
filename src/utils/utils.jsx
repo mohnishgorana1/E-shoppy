4d50e564a451ef8999ccf61bca86abd4915c5968
mohnishgorana1 <mohnishgorana1@gmail.com>
2023-10-18 10:39:33 +0530

    format price according to need

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -18,8 +18,8 @@ export const formatPrice = (price) =>{
     const RupeesAmount = new Intl.NumberFormat('en-IN', {
         style:'currency',
         currency:'INR',
-    }).format((price / 100).toFixed(2));
-    return RupeesAmount;
+    }).format((price).toFixed(2));
+    return ` ${" " +RupeesAmount}`;
 }
 
 
